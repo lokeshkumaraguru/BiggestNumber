78c4418b35736e7eac6348683972bb9a0597a51f
lokeshkumaraguru <lkumaraguru@mail.csuchico.edu>
2018-09-10 16:47:59 -0700

    Declares which of the two number is larger and stores that number into a third variable called biggest. Printed the value of biggest. Additionally, if the two numbers have the same value, print wholenumber1 in a statement: They have the same value.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -3,19 +3,36 @@
 
 int main()
 {
-  //declare variables here
+  int wholenumber1;
+  int wholenumber2;
 
   std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
+  std::cin>>wholenumber1;
 
   std::cout<<"Please enter another whole number:\n";
-  //get second input using std::cin>> again with a second variable
+  std::cin>>wholenumber2;
 
-  //determine if first or second is bigger and store that in a third variable
+  int biggest;
 
+  if(wholenumber1 <  wholenumber2)
+  {
+    biggest = wholenumber2;
+  }
+  else
+  {
+    biggest = wholenumber1;
+  }
 
-  std::cout<<"Of those two numbers, the biggest is: ";
-  //print out the variable representing the biggest number
+  if(wholenumber1 == wholenumber2)
+  {
+    std::cout<<"Both numbers have the same value: ";
+    std::cout<<wholenumber1;
+  }
+  else
+  {
+    std::cout<<"Of those two numbers, the biggest is: ";
+    std::cout<<biggest;
+  }
 
   std::cout<<std::endl<<"Thank you for playing.\n";
   
